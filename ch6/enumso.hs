e36641b9e7a196aee5285e6b6c5952ea35ff6e44
Iarek Kovtunenko <iarek.kovtunenko@emailage.com>
2020-02-14 22:55:44 -0700

    enums

---
+++ ch6/enumso.hs
@@ -0,0 +1,25 @@
+module Enumso where
+
+data DayOfWeek =
+  Mon | Tue | Wed | Thu | Fri | Sat | Sun
+  deriving Show
+
+instance Enum DayOfWeek where
+  toEnum x
+    | x `rem` 7 == 0    = Mon
+    | x `rem` 7 == 1    = Tue
+    | x `rem` 7 == 2    = Wed
+    | x `rem` 7 == 3    = Thu
+    | x `rem` 7 == 4    = Fri
+    | x `rem` 7 == 5    = Sat
+    | otherwise = Sun
+
+  fromEnum Mon = 0
+  fromEnum Tue = 1
+  fromEnum Wed = 2
+  fromEnum Thu = 3
+  fromEnum Fri = 4
+  fromEnum Sat = 5
+  fromEnum Sun = 6
+
+  
