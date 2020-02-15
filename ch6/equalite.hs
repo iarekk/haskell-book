73c1951da64897b53b35ba004d56d7b45f012584
Iarek Kovtunenko <iarek.kovtunenko@emailage.com>
2020-02-14 21:38:54 -0700

    Date equality

--- ch6/equalite.hs
+++ ch6/equalite.hs
@@ -5,5 +5,26 @@ data Sausage = Frankfurter | Bratwurst
 instance Eq Sausage where
   Frankfurter == Frankfurter = True
   Bratwurst == Bratwurst = True
-  Bratwurst == _ = False
-  Frankfurter == _ = False
+  _ == _ = False
+
+data DayOfWeek =
+  Mon | Tue | Wed | Thu | Fri | Sat | Sun
+
+instance Eq DayOfWeek where
+  (==) Mon Mon = True
+  (==) Tue Tue = True
+  (==) Wed Wed = True
+  (==) Thu Thu = True
+  (==) Fri Fri = True
+  (==) Sat Sat = True
+  (==) Sun Sun = True
+  (==) _ _     = False
+
+-- day of week and numerical day of month
+data Date =
+  Date DayOfWeek Int
+
+instance Eq Date where
+  (==) (Date dw1 dm1) (Date dw2 dm2) =
+    dw1 == dw2 
+    && dm1 == dm2
