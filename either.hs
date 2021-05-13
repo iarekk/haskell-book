f1fb7dead5b98ae310d97235890bcb6927eb4d09
Iarek Kovtunenko <iarek.kovtunenko@emailage.com>
2021-05-13 19:29:47 +0100

    either with bind

--- either.hs
+++ either.hs
@@ -5,13 +5,14 @@ import Text.Read (readMaybe)
 newtype Person = Person { age :: Int} deriving (Show)
 
 example :: String -> Either String Person
-example ageString = do
-    age <- case readMaybe ageString of
+example ageString =
+    case readMaybe ageString of
         Nothing  -> Left "Invalid age string"
         Just age -> pure age
-
-    if age < 0
-        then Left "Negative age"
-        else pure ()
-
-    pure Person { age }
+        >>=
+            (\age -> 
+                if age < 0
+                    then Left "Negative age"
+                    else pure ()
+                    >>=
+                        (\()-> pure Person { age }))
