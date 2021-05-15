3079302c37aebbf65c6d8b759dda4f70f853b279
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2021-05-15 19:24:15 +0100

    type signatures

--- either.hs
+++ either.hs
@@ -10,12 +10,15 @@ example ageString =
     >>= (\age -> checkAge age
         >>= (\()-> createPerson age))
 
+getAge :: Read b => String -> Either [Char] b
 getAge ageString = case readMaybe ageString of
        Nothing  -> Left "Invalid age string"
        Just age -> pure age
 
+checkAge :: (Ord a, Num a) => a -> Either [Char] ()
 checkAge a = if a < 0
        then Left "Negative age"
        else pure ()
 
+createPerson :: Applicative f => Int -> f Person
 createPerson a = pure Person { age = a }
