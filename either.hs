41aedb2c628d95aa3db34f7ae851909c8700633e
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2021-05-15 19:21:14 +0100

    update to the bind syntax

--- either.hs
+++ either.hs
@@ -1,18 +1,21 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
-import Text.Read (readMaybe)
+import           Text.Read (readMaybe)
 
 newtype Person = Person { age :: Int} deriving (Show)
 
 example :: String -> Either String Person
 example ageString =
-    case readMaybe ageString of
-        Nothing  -> Left "Invalid age string"
-        Just age -> pure age
-        >>=
-            (\age -> 
-                if age < 0
-                    then Left "Negative age"
-                    else pure ()
-                    >>=
-                        (\()-> pure Person { age }))
+    getAge ageString
+    >>= (\age -> checkAge age
+        >>= (\()-> createPerson age))
+
+getAge ageString = case readMaybe ageString of
+       Nothing  -> Left "Invalid age string"
+       Just age -> pure age
+
+checkAge a = if a < 0
+       then Left "Negative age"
+       else pure ()
+
+createPerson a = pure Person { age = a }
